99aff8eef5b5d673df12cfb0dbd14f71715db4b4
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-03-06 11:28:28 +0900

    chore: browser.element化

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,38 @@
 module Main exposing (Msg(..), main, update, view)
 
 import Browser
-import Html exposing (Html, button, div, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Events exposing (..)
+
+
+
+-- MAIN
 
 
 main =
-    Browser.sandbox { init = 0, update = update, view = view }
+    Browser.element
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( { count = 0 }, Cmd.none )
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { count : Int }
+
+
+
+--UPDATE
 
 
 type Msg
@@ -14,18 +40,41 @@ type Msg
     | Decrement
 
 
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            let
+                newCount =
+                    model.count + 1
+            in
+            ( { model | count = newCount }, Cmd.none )
 
         Decrement ->
-            model - 1
+            let
+                newCount =
+                    model.count - 1
+            in
+            ( { model | count = newCount }, Cmd.none )
 
 
+
+-- VIEW
+
+
+view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text (String.fromInt model) ]
+        , div [] [ text (String.fromInt model.count) ]
         , button [ onClick Increment ] [ text "+" ]
         ]
+
+
+
+-- SUBSCRIPTION
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
