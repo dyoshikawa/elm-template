2521b4304ace459fe568292a654c0fa5604cd316
dyoshikawa <yoshikawada@crunchtimer.jp>
2019-05-22 10:26:07 +0900

    fix: format

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,11 @@
+module Main exposing (Model, Msg(..), Page(..), goTo, init, main, subscriptions, update, view, viewLink)
+
 import Browser
 import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import Route exposing (Route, parse)
 import Url
-import Route exposing (parse, Route)
 
 
 
@@ -12,31 +14,34 @@ import Route exposing (parse, Route)
 
 main : Program () Model Msg
 main =
-  Browser.application
-    { init = init
-    , view = view
-    , update = update
-    , subscriptions = subscriptions
-    , onUrlChange = UrlChanged
-    , onUrlRequest = UrlRequested
-    }
+    Browser.application
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        , onUrlChange = UrlChanged
+        , onUrlRequest = UrlRequested
+        }
 
 
 
 -- MODEL
 
-type Page = TopPage | NotFound
+
+type Page
+    = TopPage
+    | NotFound
 
 
 type alias Model =
-  { key : Nav.Key
-  , page : Page
-  }
+    { key : Nav.Key
+    , page : Page
+    }
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init flags url key =
-  Model key TopPage |> goTo (Route.parse url)
+    Model key TopPage |> goTo (Route.parse url)
 
 
 
@@ -44,31 +49,34 @@ init flags url key =
 
 
 type Msg
-  = UrlRequested Browser.UrlRequest
-  | UrlChanged Url.Url
+    = UrlRequested Browser.UrlRequest
+    | UrlChanged Url.Url
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-  case msg of
-    UrlRequested urlRequest ->
-      case urlRequest of
-        Browser.Internal url ->
-          ( model, Nav.pushUrl model.key (Url.toString url) )
+    case msg of
+        UrlRequested urlRequest ->
+            case urlRequest of
+                Browser.Internal url ->
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
 
-        Browser.External href ->
-          ( model, Nav.load href )
+                Browser.External href ->
+                    ( model, Nav.load href )
 
-    UrlChanged url ->
-      goTo (Route.parse url) model
+        UrlChanged url ->
+            goTo (Route.parse url) model
 
-goTo : Maybe Route -> Model -> (Model, Cmd Msg)
+
+goTo : Maybe Route -> Model -> ( Model, Cmd Msg )
 goTo maybeRoute model =
     case maybeRoute of
         Nothing ->
-            ({model | page = NotFound}, Cmd.none)
+            ( { model | page = NotFound }, Cmd.none )
+
         Just Route.Top ->
-            ({model | page = TopPage}, Cmd.none)
+            ( { model | page = TopPage }, Cmd.none )
+
 
 
 -- SUBSCRIPTIONS
@@ -76,7 +84,7 @@ goTo maybeRoute model =
 
 subscriptions : Model -> Sub Msg
 subscriptions _ =
-  Sub.none
+    Sub.none
 
 
 
@@ -85,21 +93,26 @@ subscriptions _ =
 
 view : Model -> Browser.Document Msg
 view model =
-  { title = "URL Interceptor"
-  , body =
-      [ text "The current URL is: "
-      , ul []
-          [ viewLink "/home"
-          , viewLink "/profile"
-          , viewLink "/reviews/the-century-of-the-self"
-          , viewLink "/reviews/public-opinion"
-          , viewLink "/reviews/shah-of-shahs"
-          ]
-        -- , case model.url of
-      ]
-  }
+    { title = "URL Interceptor"
+    , body =
+        [ text "The current URL is: "
+        , ul []
+            [ viewLink "/home"
+            , viewLink "/profile"
+            , viewLink "/reviews/the-century-of-the-self"
+            , viewLink "/reviews/public-opinion"
+            , viewLink "/reviews/shah-of-shahs"
+            ]
+        , case model.page of
+            NotFound ->
+                h1 [] [ text "NotFound" ]
+
+            TopPage ->
+                h1 [] [ text "TopPage" ]
+        ]
+    }
 
 
 viewLink : String -> Html msg
 viewLink path =
-  li [] [ a [ href path ] [ text path ] ]
+    li [] [ a [ href path ] [ text path ] ]
